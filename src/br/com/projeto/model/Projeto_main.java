1597501efdb242978582a935df842d49dbc92926
Guilherme <oguilhermeleitefelix@gmail.com>
2021-07-11 12:39:04 -0300

    Classe main criada

---
+++ src/br/com/projeto/model/Projeto_main.java
@@ -0,0 +1,10 @@
+
+package br.com.projeto.model;
+
+import br.com.projeto.view.Autenticador;
+
+public class Projeto_main {
+    public static void main(String[] args) {
+        new Autenticador().setVisible(true);
+    }
+}
